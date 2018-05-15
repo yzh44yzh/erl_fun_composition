4cb146961c80aa53e87ce4acd478a2c35fcd003c
Yuriy Zhloba <y_zhloba@wargaming.net>
2018-05-15 07:38:55 +0300

    refactor haskell version 4

--- hs_src/Main4.hs
+++ hs_src/Main4.hs
@@ -15,23 +15,19 @@ handle_create_order json_data =
 
 validate_cat :: String -> Either BS.ValidationError (String, BS.Cat)
 validate_cat json_data =
-  case BS.validate_cat "Tihon" of
-    Left err -> Left err
-    Right cat -> Right (json_data, cat)
+  BS.validate_cat "Tihon"
+  >>= \cat -> Right (json_data, cat)
 
 
 validate_address :: (String, BS.Cat) -> Either BS.ValidationError (String, BS.Cat, BS.Address)
 validate_address (json_data, cat) =
-  case BS.validate_address "Coolcat str 7/42 Minsk Belarus" of
-    Left err -> Left err
-    Right addr -> Right (json_data, cat, addr)
+  BS.validate_address "Coolcat str 7/42 Minsk Belarus"
+  >>= \addr -> Right (json_data, cat, addr)
 
 
 validate_books :: (String, BS.Cat, BS.Address) -> Either BS.ValidationError (BS.Cat, BS.Address, [BS.Book])
 validate_books (json_data, cat, addr) =
-  case get_books of
-    Left err -> Left err
-    Right books -> Right (cat, addr, books)
+  get_books >>= \books -> Right (cat, addr, books)
   where
     get_books = sequence $ map (\(t, a) -> BS.get_book t a) book_strs
     book_strs = [ ("Domain Modeling Made Functional", "Scott Wlaschin")
